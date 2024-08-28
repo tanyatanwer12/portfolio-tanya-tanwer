ea2e39a176969eef9f874e7637826811f2903b35
tanyatanwer12 <125805397+tanyatanwer12@users.noreply.github.com>
2024-08-28 21:21:15 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,222 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Blog", link: "#blogs" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className:
+      "lg:col-span-2 md:col-span-4 md:row-span-2 lg:min-h-[60vh] mt-44 ml-20 mr-20 bottom-24",
+    imgClassName: "",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className:
+      "lg:col-span-1 md:col-span-2 md:row-span-1 mt-20 mr-5 right-12 bottom-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className:
+      "lg:col-span-1 md:col-span-2 md:row-span-1 mt-20 right-12 mr-5 bottom-24",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className:
+      "lg:col-span-1 md:col-span-2 md:row-span-1 mt-36 ml-25 mr-30 bottom-60 left-20 ",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-2 md:row-span-1 mt-18 left-20 mr-36 bottom-24",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className:
+      "lg:col-span-1 md:col-span-2 md:row-span-1 mt-36 ml-25 mr-30 bottom-96 left-20 ",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Coffee-Beans a Coffee Shop site",
+    des: "Discover the perfect blend of technology and taste with our user-friendly frontend design, ensuring a seamless and delightful browsing experience for all coffee lovers.",
+    img: "/coffee-shop.png",
+    iconLists: ["/html.png", "/javascript.png", "/tailwind.png"],
+    link: "coffee-shop-folder.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Amazon clone",
+    des: "Your Ultimate Online Shopping Experience, Brought to Life with Cutting-Edge Front-End Design.",
+    img: "/amazon.png",
+    iconLists: ["/html.png", "/javascript.png", "/tailwind.png"],
+    link: "https://amazon-clone-ochre-one-81.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://imaginify-oa2dt6q6c-js-mastery-pro.vercel.app",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://iphone-doc.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "GSAP (Green Sock Animation Platform) is a powerful JavaScript library used for creating complex animations and interactions on the web. With its robust features and ease of use, GSAP has become a go-to tool for developers and designers looking to elevate their web projects. In this blog, we’ll delve into the world of GSAP, exploring its features, benefits, and usage.",
+    name: "Tanya Tanwer",
+    title: "",
+  },
+  {
+    quote:
+      "Tailwind CSS is a utility-first CSS framework that enables rapid building of modern websites without leaving HTML. It provides low-level utility classes to build custom designs directly in HTML, streamlining web development. With its pre-designed utility classes, Tailwind CSS simpl ifies responsive design, layout, spacing, typography, and more.",
+    name: "Tanya Tanwer",
+    title: "",
+  },
+  {
+    quote:
+      "GSAP (Green Sock Animation Platform) is a powerful JavaScript library used for creating complex animations and interactions on the web. With its robust features and ease of use, GSAP has become a go-to tool for developers and designers looking to elevate their web projects. In this blog, we’ll delve into the world of GSAP, exploring its features, benefits, and usage.",
+    name: "Tanya Tanwer",
+    title: "",
+  },
+  {
+    quote:
+      "Tailwind CSS is a utility-first CSS framework that enables rapid building of modern websites without leaving HTML. It provides low-level utility classes to build custom designs directly in HTML, streamlining web development. With its pre-designed utility classes, Tailwind CSS simpl ifies responsive design, layout, spacing, typography, and more.",
+    name: "Tanya Tanwer",
+    title: "",
+  },
+  {
+    quote:
+      "GSAP (Green Sock Animation Platform) is a powerful JavaScript library used for creating complex animations and interactions on the web. With its robust features and ease of use, GSAP has become a go-to tool for developers and designers looking to elevate their web projects. In this blog, we’ll delve into the world of GSAP, exploring its features, benefits, and usage.",
+    name: "Tanya Tanwer",
+    title: "",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Microsoft student ambassador",
+    desc: "Unlocking student potential, driving innovation, and shaping the future with Microsoft technology and expertise.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web project for a client, from initial concept to deployment on web stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Vice Chair-IEEE WIE",
+    desc: "Amplifying technical excellence, fostering collaboration, and driving innovation in the next generation of leaders..",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
