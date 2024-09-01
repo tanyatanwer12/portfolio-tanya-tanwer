88771849c6a0cf682c81004145e4bf64763600bc
tanyatanwer12 <125805397+tanyatanwer12@users.noreply.github.com>
2024-09-01 13:32:02 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
             <div key={profile.id}>
               <img
                 src={profile.img}
-                alt={profile.id}
+                alt={String(profile.id)}
                 width={20}
                 height={20}
                 className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
